9381dbfe3f8a125a29ec7b171cff13a1eeeea3fd
DANIEL LOZANO BORGES P�RES <25254373@educ123.sp.senai.br>
2026-05-08 09:43:31 -0300

    atualizações no css

--- partials/header.php
+++ partials/header.php
@@ -1,10 +1,15 @@
 <header>
     <nav>
-        <p class="titulo">Daniel Lozano</p>
         <ul>
+            <p class="titulo">WebPage</p>
             <li><a href="">Teste</a></li>
             <li><a href="">Teste</a></li>
             <li><a href="">Teste</a></li>
-        </ul>        
+        </ul>
+        <ul>
+            <button>Nova Figurinha +</button>
+            <img src="/Layout/img/sino.png" alt="" class="invert">
+            <img src="/Layout/img/igor.png" alt="" class="perfil">
+        </ul>
     </nav>
 </header>
